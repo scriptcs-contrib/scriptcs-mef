9e32fde403a171e75622e4ad4e5d43076bad9173
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2015-03-25 21:31:02 +0100

    Add scripts folder initialization

--- sample/ScriptCompositionSample/Program.cs
+++ sample/ScriptCompositionSample/Program.cs
@@ -1,6 +1,7 @@
 ﻿using ScriptCs.ComponentModel.Composition;
 using System.ComponentModel.Composition;
 using System.ComponentModel.Composition.Hosting;
+using System.IO;
 
 namespace ScriptCompositionSample
 {
@@ -19,12 +20,26 @@ namespace ScriptCompositionSample
 
         public void Compose()
         {
+            // You can add script by script
             //var catalog = new ScriptCsCatalog(new[] { "Test.csx" }, typeof(IGreeter));
+
+            // Or an entire folder
+            InitializeScripts();
             var catalog = new ScriptCsCatalog("Scripts", typeof(IGreeter));
             var container = new CompositionContainer(catalog);
             var batch = new CompositionBatch();
             batch.AddPart(this);
             container.Compose(batch);
+        }
+
+        private void InitializeScripts()
+        {
+            if (!Directory.Exists("Scripts"))
+            {
+                Directory.CreateDirectory("Scripts");
+            }
+
+            File.Copy("Test.csx", "Scripts/Test.csx");
         }
     }
 }
