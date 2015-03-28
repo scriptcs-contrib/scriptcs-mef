dd3ad9fd8ea054122cd81b24787fcd4d49d5d8e1
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2015-03-28 08:29:32 +0100

    Run code coverage only if it's no PR

---
+++ RunTests.ps1
@@ -0,0 +1,8 @@
+if($env:APPVEYOR_PULL_REQUEST_NUMBER -eq $null) {
+    OpenCover.Console.exe -register:user -filter:"+[ScriptCs.ComponentModel.Composition]*" -excludebyattribute:*.ExcludeFromCodeCoverage* -target:"$env:xunit20\xunit.console.x86" -targetargs:""src\ScriptCs.ComponentModel.Composition.Test\bin\Release\ScriptCs.ComponentModel.Composition.Test.dll" -noshadow -appveyor" -output:coverage.xml -returntargetcode
+    coveralls.net --opencover coverage.xml
+}
+else
+{
+    $env:xunit20\xunit.console.x86 "src\ScriptCs.ComponentModel.Composition.Test\bin\Release\ScriptCs.ComponentModel.Composition.Test.dll" -noshadow -appveyor
+}
