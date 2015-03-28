0f2760c3d4eec4b91f82ed4d936247e68aaa09fa
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2015-03-28 09:21:47 +0100

    Fix xunit args in opencover command

--- RunTests.ps1
+++ RunTests.ps1
@@ -1,5 +1,5 @@
 if($env:APPVEYOR_PULL_REQUEST_NUMBER -eq $null) {
-    OpenCover.Console.exe -register:user -filter:"+[ScriptCs.ComponentModel.Composition]*" -excludebyattribute:*.ExcludeFromCodeCoverage* -target:"$env:xunit20\xunit.console.x86.exe" -targetargs:""src\ScriptCs.ComponentModel.Composition.Test\bin\Release\ScriptCs.ComponentModel.Composition.Test.dll" -noshadow -appveyor" -output:coverage.xml -returntargetcode
+    OpenCover.Console.exe -register:user -filter:"+[ScriptCs.ComponentModel.Composition]*" -excludebyattribute:*.ExcludeFromCodeCoverage* -target:"$env:xunit20\xunit.console.x86.exe" -targetargs:"`"src\ScriptCs.ComponentModel.Composition.Test\bin\Release\ScriptCs.ComponentModel.Composition.Test.dll`" -noshadow -appveyor" -output:coverage.xml -returntargetcode
     coveralls.net --opencover coverage.xml
 }
 else
