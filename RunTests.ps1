21ded87241e6b383d5154a46c24318aedafe33de
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2017-02-02 19:01:33 +0100

    Replace coveralls with codecov

--- RunTests.ps1
+++ RunTests.ps1
@@ -1,20 +1,8 @@
-if($env:APPVEYOR_PULL_REQUEST_NUMBER -eq $null) {
-    OpenCover.Console.exe -register:user -filter:"+[ScriptCs.ComponentModel.Composition]*" -excludebyattribute:*.ExcludeFromCodeCoverage* -target:"$env:xunit20\xunit.console.x86.exe" -targetargs:"`"src\ScriptCs.ComponentModel.Composition.Test\bin\Release\ScriptCs.ComponentModel.Composition.Test.dll`" -noshadow -appveyor" -output:coverage.xml -returntargetcode
-    if ($lastExitCode -ne 0)
-    {
-        Write-Error "Tests have failed"
-        exit -1
-    }
-    Write-Information "Tests ok"
-    coveralls.net --opencover coverage.xml
-}
-else
+OpenCover.Console.exe -register:user -filter:"+[ScriptCs.ComponentModel.Composition]*" -excludebyattribute:*.ExcludeFromCodeCoverage* -target:"$env:xunit20\xunit.console.x86.exe" -targetargs:"`"src\ScriptCs.ComponentModel.Composition.Test\bin\Release\ScriptCs.ComponentModel.Composition.Test.dll`" -noshadow -appveyor" -output:coverage.xml -returntargetcode
+if ($lastExitCode -ne 0)
 {
-    & "$env:xunit20\xunit.console.x86" "src\ScriptCs.ComponentModel.Composition.Test\bin\Release\ScriptCs.ComponentModel.Composition.Test.dll" -noshadow -appveyor
-    if ($lastExitCode -ne 0)
-    {
-        Write-Error "Tests have failed"
-        exit -1
-    }
-    Write-Information "Tests ok"
+    Write-Error "Tests have failed"
+    exit -1
 }
+Write-Information "Tests ok"
+codecov -f coverage.xml
