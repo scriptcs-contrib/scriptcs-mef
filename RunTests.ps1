14220cf259b24f5cfa5cbd24245298976fd4ea2b
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2017-02-02 07:36:48 +0100

    Fix fail build on test fail

--- RunTests.ps1
+++ RunTests.ps1
@@ -2,11 +2,17 @@ if($env:APPVEYOR_PULL_REQUEST_NUMBER -eq $null) {
     OpenCover.Console.exe -register:user -filter:"+[ScriptCs.ComponentModel.Composition]*" -excludebyattribute:*.ExcludeFromCodeCoverage* -target:"$env:xunit20\xunit.console.x86.exe" -targetargs:"`"src\ScriptCs.ComponentModel.Composition.Test\bin\Release\ScriptCs.ComponentModel.Composition.Test.dll`" -noshadow -appveyor" -output:coverage.xml -returntargetcode
     if ($lastExitCode -ne 0)
     {
-        exit -1
+        Write-Error "Tests have failed"
     }
+    Write-Information "Tests ok"
     coveralls.net --opencover coverage.xml
 }
 else
 {
     & "$env:xunit20\xunit.console.x86" "src\ScriptCs.ComponentModel.Composition.Test\bin\Release\ScriptCs.ComponentModel.Composition.Test.dll" -noshadow -appveyor
+    if ($lastExitCode -ne 0)
+    {
+        Write-Error "Tests have failed"
+    }
+    Write-Information "Tests ok"
 }
