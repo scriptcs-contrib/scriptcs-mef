152a18840b143cd770601bf3c8cdb6e586574685
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2017-02-02 19:21:59 +0100

    separate test and code coverage

--- RunTests.ps1
+++
@@ -1,8 +0,0 @@
-OpenCover.Console.exe -register:user -filter:"+[ScriptCs.ComponentModel.Composition]*" -excludebyattribute:*.ExcludeFromCodeCoverage* -target:"$env:xunit20\xunit.console.x86.exe" -targetargs:"`"src\ScriptCs.ComponentModel.Composition.Test\bin\Release\ScriptCs.ComponentModel.Composition.Test.dll`" -noshadow -appveyor" -output:coverage.xml -returntargetcode
-if ($lastExitCode -ne 0)
-{
-    Write-Error "Tests have failed"
-    exit -1
-}
-Write-Information "Tests ok"
-codecov -f coverage.xml
