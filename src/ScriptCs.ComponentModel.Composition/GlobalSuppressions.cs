97ac1913a960529381eff6d429c52fad3f22bc05
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2017-02-03 19:16:13 +0100

    :rotating_light: remove warning on possible null

---
+++ src/ScriptCs.ComponentModel.Composition/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Sonar Bug", "S2259:Null pointers should not be dereferenced", Justification = "Verified", Scope = "member", Target = "~M:ScriptCs.ComponentModel.Composition.ScriptCsCatalog.ExecuteScripts(System.String[])~System.ComponentModel.Composition.Hosting.AssemblyCatalog")]
+
