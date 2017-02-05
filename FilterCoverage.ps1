a44a5787a57014913a4483c80798a4d410cb4330
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2017-02-05 14:56:03 +0100

    trying with another file

--- FilterCoverage.ps1
+++ FilterCoverage.ps1
@@ -1,8 +1,7 @@
 [xml]$xml = Get-Content "coverage.xml" -Encoding UTF8
-Write-Host ($xml | Format-Table | Out-String)
 
 $xml.CoverageSession.Modules.Module | % { if ($_ -and $_.ModuleName -and $_.ModuleName.StartsWith('ℛ*')) { $_.ParentNode.RemoveChild($_) } }
 
-$fileName = "$env:APPVEYOR_BUILD_FOLDER\coverage.xml";
-[System.IO.File]::WriteAllLines("$env:APPVEYOR_BUILD_FOLDER\coverage.xml", $xml.OuterXml)
+$fileName = "$env:APPVEYOR_BUILD_FOLDER\filtered-coverage.xml";
+[System.IO.File]::WriteAllLines($fileName, $xml.OuterXml)
 Write-Host "file saved at '$fileName'"
