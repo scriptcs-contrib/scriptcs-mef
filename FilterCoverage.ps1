e1035d5b930135c1c37a4d5a24ced9d65f5493a4
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2017-02-05 15:03:11 +0100

    more traces

--- FilterCoverage.ps1
+++ FilterCoverage.ps1
@@ -1,6 +1,6 @@
 [xml]$xml = Get-Content "coverage.xml" -Encoding UTF8
 Write-Host $xml.OuterXml
-$xml.CoverageSession.Modules.Module | % { if ($_ -and $_.ModuleName -and $_.ModuleName.StartsWith('ℛ*')) { $_.ParentNode.RemoveChild($_) } }
+$xml.CoverageSession.Modules.Module | % { Write-Host "$_ is examined"; if ($_ -and $_.ModuleName -and $_.ModuleName.StartsWith('ℛ*')) { Write-Host "$_.ModuleName removed"; $_.ParentNode.RemoveChild($_) } }
 
 $fileName = "$env:APPVEYOR_BUILD_FOLDER\filtered-coverage.xml";
 [System.IO.File]::WriteAllLines($fileName, $xml.OuterXml)
