3b769f57b7fd1d305fbc8ef8e3886fefe94dd77f
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2017-02-05 16:59:34 +0100

    Replace coveralls with codecov (#13)
    Replace coveralls with codecov

---
+++ FilterCoverage.fsx
@@ -0,0 +1,8 @@
+let doc = new System.Xml.XmlDocument() in doc.Load "coverage.xml"
+doc.SelectNodes "//ModuleName[starts-with(., 'ℛ')]/.."
+    |> Seq.cast<System.Xml.XmlNode>
+    |> Seq.iter (fun node -> node.ParentNode.RemoveChild(node) |> ignore)
+    |> ignore
+let filteredFileName = System.String.Concat(System.Environment.GetEnvironmentVariable("APPVEYOR_BUILD_FOLDER"), "/filtered-coverage.xml")
+System.Console.WriteLine(filteredFileName)
+System.IO.File.WriteAllLines(filteredFileName, doc.OuterXml.Split(' '))
