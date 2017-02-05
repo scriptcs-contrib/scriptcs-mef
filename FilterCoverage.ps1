2ef764eb5c9f525d74f9e03b2063a6ce4af3e0b4
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2017-02-05 14:47:56 +0100

    Filter ℛ* and save as uts8 without BOM

--- FilterCoverage.ps1
+++ FilterCoverage.ps1
@@ -1,2 +1,8 @@
-$MyFile = Get-Content "coverage.xml"
-[System.IO.File]::WriteAllLines("$env:APPVEYOR_BUILD_FOLDER\coverage.xml", $MyFile)
+[xml]$xml = Get-Content "coverage.xml" -Encoding UTF8
+Write-Host ($xml | Format-Table | Out-String)
+
+$xml.CoverageSession.Modules.Module | % { if ($_ -and $_.ModuleName -and $_.ModuleName.StartsWith('ℛ*')) { $_.ParentNode.RemoveChild($_) } }
+
+$fileName = "$env:APPVEYOR_BUILD_FOLDER\coverage.xml";
+[System.IO.File]::WriteAllLines("$env:APPVEYOR_BUILD_FOLDER\coverage.xml", $xml.OuterXml)
+Write-Host "file saved at '$fileName'"
