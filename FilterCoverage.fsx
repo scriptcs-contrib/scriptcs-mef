de887da926901e10b65d0b5e6cb8b8184cd94998
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2017-02-05 16:44:31 +0100

    missing slash

--- FilterCoverage.fsx
+++ FilterCoverage.fsx
@@ -3,6 +3,6 @@ doc.SelectNodes "//ModuleName[starts-with(., 'ℛ')]/.."
     |> Seq.cast<System.Xml.XmlNode>
     |> Seq.iter (fun node -> node.ParentNode.RemoveChild(node) |> ignore)
     |> ignore
-let filteredFileName = System.String.Concat(System.Environment.GetEnvironmentVariable("APPVEYOR_BUILD_FOLDER"), "filtered-coverage.xml")
+let filteredFileName = System.String.Concat(System.Environment.GetEnvironmentVariable("APPVEYOR_BUILD_FOLDER"), "/filtered-coverage.xml")
 System.Console.WriteLine(filteredFileName)
 System.IO.File.WriteAllLines(filteredFileName, doc.OuterXml.Split(' '))
