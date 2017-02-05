bd8bd836186c531775b38068e2204c2218546a63
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2017-02-05 16:03:31 +0100

    space needed

--- FilterCoverage.fsx
+++ FilterCoverage.fsx
@@ -3,4 +3,4 @@ doc.SelectNodes "//ModuleName[starts-with(., 'ℛ')]/.."
     |> Seq.cast<System.Xml.XmlNode>
     |> Seq.iter (fun node -> node.ParentNode.RemoveChild(node) |> ignore)
     |> ignore
-System.IO.File.WriteAllLines("C:\Users\man-p\Downloads\filtered-coverage.xml", doc.OuterXml.Split(''))
+System.IO.File.WriteAllLines("C:\Users\man-p\Downloads\filtered-coverage.xml", doc.OuterXml.Split(' '))
