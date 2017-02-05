41955c65db6c70903e222f99aa0849c59216da47
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2017-02-05 14:43:39 +0100

    Only change encoding

--- FilterCoverage.ps1
+++ FilterCoverage.ps1
@@ -1,8 +1,2 @@
-[xml]$xml = Get-Content "coverage.xml"
-Write-Host ($xml | Format-Table | Out-String)
-
-$xml.CoverageSession.Modules.Module | % { if ($_ -and $_.ModuleName -and $_.ModuleName.StartsWith('â„›*')) { $_.ParentNode.RemoveChild($_) } }
-
-$fileName = "$env:APPVEYOR_BUILD_FOLDER\coverage.xml";
-$xml.Save($fileName)
-Write-Host "file saved at '$fileName'"
+$MyFile = Get-Content "coverage.xml"
+[System.IO.File]::WriteAllLines("$env:APPVEYOR_BUILD_FOLDER\coverage.xml", $MyFile)
