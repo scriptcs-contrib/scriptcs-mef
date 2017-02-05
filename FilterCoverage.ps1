4a26e58fcf3063518da68e950bb386a4cf3666a5
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2017-02-05 16:50:44 +0100

    FREE AT LAST

--- FilterCoverage.ps1
+++
@@ -1,4 +0,0 @@
-[xml]$xml = Get-Content "coverage.xml" -Encoding UTF8
-$xml.SelectNodes("//ModuleName[starts-with(., 'ℛ')]/..") | % { $_.ParentNode.RemoveChild($_) };
-
-[System.IO.File]::WriteAllLines("R_BUILD_FOLDER\filtered-coverage.xml", $xml.OuterXml)
