2f011ea87a304347eb9788f5a7f8edd1f76d25f4
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2017-02-05 10:23:02 +0100

    With a script

---
+++ FilterCoverage.ps1
@@ -0,0 +1,5 @@
+[xml]$xml = Get-Content "coverage.xml" -Encoding UTF8
+
+$xml.CoverageSession.Modules.Module | % { if ($_ -and $_.ModuleName -and $_.ModuleName.StartsWith('ℛ*')) { $_.ParentNode.RemoveChild($_) } }
+
+$xml.Save("$env:APPVEYOR_BUILD_FOLDER\filtered-coverage.xml")
