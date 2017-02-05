7370b2d92f64c645b63041750cc206e7d2ce5620
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2017-02-05 10:48:16 +0100

    fix encoding issue

--- FilterCoverage.ps1
+++ FilterCoverage.ps1
@@ -1,7 +1,7 @@
-[xml]$xml = Get-Content "coverage.xml" -Encoding UTF8
+[xml]$xml = Get-Content "coverage.xml"
 Write-Host ($xml | Format-Table | Out-String)
 
-$xml.CoverageSession.Modules.Module | % { if ($_ -and $_.ModuleName -and $_.ModuleName.StartsWith('ℛ*')) { $_.ParentNode.RemoveChild($_) } }
+$xml.CoverageSession.Modules.Module | % { if ($_ -and $_.ModuleName -and $_.ModuleName.StartsWith('â„›*')) { $_.ParentNode.RemoveChild($_) } }
 
 $fileName = "$env:APPVEYOR_BUILD_FOLDER\coverage.xml";
 $xml.Save($fileName)
